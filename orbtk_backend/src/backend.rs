4880b6571a1e9471372ed57cfdba0789ae86f03c
arctic-alpaca <67190338+arctic-alpaca@users.noreply.github.com>
2020-09-30 14:13:57 +0200

    switch to RefCell to avoid changes to plotters api

--- orbtk_backend/src/backend.rs
+++ orbtk_backend/src/backend.rs
@@ -18,7 +18,7 @@ impl std::error::Error for OrbtkError {}
 
 /// The drawing backend that is backed with a Cairo context
 pub struct OrbtkBackend<'a> {
-    render_ctx: &'a mut RenderContext2D,
+    render_ctx: RefCell<&'a mut RenderContext2D>,
     width: u32,
     height: u32,
 }
@@ -32,7 +32,7 @@ impl<'a> OrbtkBackend<'a> {
         let bytes = include_bytes!("Roboto-Regular.ttf");
         render_ctx.register_font("Roboto-Regular", bytes);
         let ret = Self {
-            render_ctx,
+            render_ctx: RefCell::new(render_ctx),
             width: width as u32,
             height: height as u32,
         };
@@ -70,13 +70,12 @@ impl<'a> DrawingBackend for OrbtkBackend<'a> {
         color: BackendColor,
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
         println!("draw_pixel");
-        self.render_ctx.begin_path();
-        self.render_ctx.set_alpha(color.alpha as f32);
-        self.render_ctx
-            .set_fill_style(utils::Brush::SolidColor(self.color_change(&color)));
+        let mut render_ctx = self.render_ctx.borrow_mut();
+        render_ctx.begin_path();
+        render_ctx.set_alpha(color.alpha as f32);
+        render_ctx.set_fill_style(utils::Brush::SolidColor(self.color_change(&color)));
 
-        self.render_ctx
-            .fill_rect(point.0 as f64, point.1 as f64, 1.0, 1.0);
+        render_ctx.fill_rect(point.0 as f64, point.1 as f64, 1.0, 1.0);
         Ok(())
     }
 
@@ -86,17 +85,17 @@ impl<'a> DrawingBackend for OrbtkBackend<'a> {
         to: (i32, i32),
         style: &S,
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
+        let mut render_ctx = self.render_ctx.borrow_mut();
         println!("draw_line");
-        self.render_ctx.begin_path();
-        self.render_ctx.set_line_width(style.stroke_width() as f64);
-        self.render_ctx.set_alpha(style.color().alpha as f32);
-        self.render_ctx
-            .set_stroke_style(utils::Brush::SolidColor(self.color_change(&style.color())));
+        render_ctx.begin_path();
+        render_ctx.set_line_width(style.stroke_width() as f64);
+        render_ctx.set_alpha(style.color().alpha as f32);
+        render_ctx.set_stroke_style(utils::Brush::SolidColor(self.color_change(&style.color())));
 
-        self.render_ctx.move_to(from.0 as f64, from.1 as f64);
-        self.render_ctx.line_to(to.0 as f64, to.1 as f64);
+        render_ctx.move_to(from.0 as f64, from.1 as f64);
+        render_ctx.line_to(to.0 as f64, to.1 as f64);
 
-        self.render_ctx.stroke();
+        render_ctx.stroke();
 
         Ok(())
     }
@@ -108,22 +107,20 @@ impl<'a> DrawingBackend for OrbtkBackend<'a> {
         style: &S,
         fill: bool,
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
+        let mut render_ctx = self.render_ctx.borrow_mut();
         println!("draw_rect");
-        self.render_ctx.begin_path();
-        self.render_ctx.set_alpha(style.color().alpha as f32);
+        render_ctx.begin_path();
+        render_ctx.set_alpha(style.color().alpha as f32);
         let width = (bottom_right.0 - upper_left.0) as f64;
         let height = (bottom_right.1 - upper_left.1) as f64;
         if fill {
-            self.render_ctx
-                .set_fill_style(utils::Brush::SolidColor(self.color_change(&style.color())));
-            self.render_ctx
-                .fill_rect(upper_left.0 as f64, upper_left.1 as f64, width, height);
+            render_ctx.set_fill_style(utils::Brush::SolidColor(self.color_change(&style.color())));
+            render_ctx.fill_rect(upper_left.0 as f64, upper_left.1 as f64, width, height);
         } else {
-            self.render_ctx.set_line_width(style.stroke_width() as f64);
-            self.render_ctx
+            render_ctx.set_line_width(style.stroke_width() as f64);
+            render_ctx
                 .set_stroke_style(utils::Brush::SolidColor(self.color_change(&style.color())));
-            self.render_ctx
-                .stroke_rect(upper_left.0 as f64, upper_left.1 as f64, width, height);
+            render_ctx.stroke_rect(upper_left.0 as f64, upper_left.1 as f64, width, height);
         }
 
         Ok(())
@@ -134,23 +131,23 @@ impl<'a> DrawingBackend for OrbtkBackend<'a> {
         path: I,
         style: &S,
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
+        let mut render_ctx = self.render_ctx.borrow_mut();
         println!("draw_path");
-        self.render_ctx.begin_path();
-        self.render_ctx.set_line_width(style.stroke_width() as f64);
-        self.render_ctx.set_alpha(style.color().alpha as f32);
-        self.render_ctx
-            .set_stroke_style(utils::Brush::SolidColor(self.color_change(&style.color())));
+        render_ctx.begin_path();
+        render_ctx.set_line_width(style.stroke_width() as f64);
+        render_ctx.set_alpha(style.color().alpha as f32);
+        render_ctx.set_stroke_style(utils::Brush::SolidColor(self.color_change(&style.color())));
 
         let iterator = path.into_iter();
         for (index, point) in iterator.enumerate() {
             if index == 0 {
-                self.render_ctx.move_to(point.0 as f64, point.1 as f64);
+                render_ctx.move_to(point.0 as f64, point.1 as f64);
             } else {
-                self.render_ctx.line_to(point.0 as f64, point.1 as f64);
+                render_ctx.line_to(point.0 as f64, point.1 as f64);
             }
         }
 
-        self.render_ctx.stroke();
+        render_ctx.stroke();
 
         Ok(())
     }
@@ -162,33 +159,33 @@ impl<'a> DrawingBackend for OrbtkBackend<'a> {
         style: &S,
         fill: bool,
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
+        let mut render_ctx = self.render_ctx.borrow_mut();
         println!("draw_circle");
-        self.render_ctx.begin_path();
-        self.render_ctx.set_alpha(style.color().alpha as f32);
+        render_ctx.begin_path();
+        render_ctx.set_alpha(style.color().alpha as f32);
 
         if fill {
-            self.render_ctx
-                .set_fill_style(utils::Brush::SolidColor(self.color_change(&style.color())));
-            self.render_ctx.arc(
+            render_ctx.set_fill_style(utils::Brush::SolidColor(self.color_change(&style.color())));
+            render_ctx.arc(
                 center.0 as f64,
                 center.1 as f64,
                 radius as f64,
                 0.0,
                 std::f64::consts::PI * 2.0,
             );
-            self.render_ctx.fill();
+            render_ctx.fill();
         } else {
-            self.render_ctx
+            render_ctx
                 .set_stroke_style(utils::Brush::SolidColor(self.color_change(&style.color())));
-            self.render_ctx.set_line_width(style.stroke_width() as f64);
-            self.render_ctx.arc(
+            render_ctx.set_line_width(style.stroke_width() as f64);
+            render_ctx.arc(
                 center.0 as f64,
                 center.1 as f64,
                 radius as f64,
                 0.0,
                 std::f64::consts::PI * 2.0,
             );
-            self.render_ctx.stroke();
+            render_ctx.stroke();
         }
 
         Ok(())
@@ -199,20 +196,20 @@ impl<'a> DrawingBackend for OrbtkBackend<'a> {
         vert: I,
         style: &S,
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
+        let mut render_ctx = self.render_ctx.borrow_mut();
         println!("fill_polygon");
-        self.render_ctx.begin_path();
-        self.render_ctx.set_alpha(style.color().alpha as f32);
-        self.render_ctx
-            .set_fill_style(utils::Brush::SolidColor(self.color_change(&style.color())));
+        render_ctx.begin_path();
+        render_ctx.set_alpha(style.color().alpha as f32);
+        render_ctx.set_fill_style(utils::Brush::SolidColor(self.color_change(&style.color())));
         for (index, point) in vert.into_iter().enumerate() {
             if index == 0 {
-                self.render_ctx.move_to(point.0 as f64, point.1 as f64);
+                render_ctx.move_to(point.0 as f64, point.1 as f64);
             } else {
-                self.render_ctx.line_to(point.0 as f64, point.1 as f64);
+                render_ctx.line_to(point.0 as f64, point.1 as f64);
             }
         }
-        self.render_ctx.close_path();
-        self.render_ctx.fill();
+        render_ctx.close_path();
+        render_ctx.fill();
 
         Ok(())
     }
@@ -223,6 +220,7 @@ impl<'a> DrawingBackend for OrbtkBackend<'a> {
         style: &TStyle,
         pos: (i32, i32),
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
+        let mut render_ctx = self.render_ctx.borrow_mut();
         let (mut x, mut y) = (pos.0, pos.1);
         /*
                 let mut degree = match style.transform() {
@@ -234,8 +232,8 @@ impl<'a> DrawingBackend for OrbtkBackend<'a> {
                     * std::f64::consts::PI;
 
                 if degree != 0.0 {
-                    self.render_ctx.save();
-                    self.render_ctx.set_transform(
+                    render_ctx.save();
+                    render_ctx.set_transform(
                         degree.cos(),
                         -degree.sin(),
                         degree.sin(),
@@ -247,14 +245,13 @@ impl<'a> DrawingBackend for OrbtkBackend<'a> {
                     y = 0;
                 }
         */
-        self.render_ctx.begin_path();
-        self.render_ctx.set_alpha(style.color().alpha as f32);
-        self.render_ctx
-            .set_fill_style(utils::Brush::SolidColor(self.color_change(&style.color())));
+        render_ctx.begin_path();
+        render_ctx.set_alpha(style.color().alpha as f32);
+        render_ctx.set_fill_style(utils::Brush::SolidColor(self.color_change(&style.color())));
 
-        self.render_ctx.set_font_size(style.size());
-        self.render_ctx.set_font_family("Roboto-Regular");
-        let metrics = self.render_ctx.measure_text(text);
+        render_ctx.set_font_size(style.size());
+        render_ctx.set_font_family("Roboto-Regular");
+        let metrics = render_ctx.measure_text(text);
 
         let dx = match style.anchor().h_pos {
             HPos::Left => 0.0,
@@ -267,25 +264,22 @@ impl<'a> DrawingBackend for OrbtkBackend<'a> {
             VPos::Bottom => -metrics.height,
         };
 
-        self.render_ctx
-            .fill_text(text, f64::from(x) + dx, f64::from(y) + dy);
+        render_ctx.fill_text(text, f64::from(x) + dx, f64::from(y) + dy);
         /*
-        if degree == 0.0 {
-            self.render_ctx.restore();
-        }
+                if degree == 0.0 {
+                    render_ctx.restore();
+                }
         */
-
         Ok(())
     }
 
     fn estimate_text_size<TStyle: BackendTextStyle>(
-        &mut self,
+        &self,
         text: &str,
         style: &TStyle,
     ) -> Result<(u32, u32), DrawingErrorKind<Self::ErrorType>> {
-        let text_metrics = self
-            .render_ctx
-            .measure(text, style.size(), "Roboto-Regular");
+        let mut render_ctx = self.render_ctx.borrow_mut();
+        let text_metrics = render_ctx.measure(text, style.size(), "Roboto-Regular");
         Ok((text_metrics.width as u32, text_metrics.height as u32))
     }
 }
